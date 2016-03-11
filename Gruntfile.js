9f8dcbb2ef9ba95b95a8d85efed129145bb3b984
Josh Pollock <jpollock412@gmail.com>
2016-03-10 22:32:32 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,211 @@
+module.exports = function (grunt) {
+
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg     : grunt.file.readJSON( 'package.json' ),
+        shell: {
+            composer: {
+                command: 'composer update --no-dev --optimize-autoloader --prefer-dist'
+            },
+            bower: {
+                command: 'bower update'
+            },
+            activate : {
+                command: 'composer update --no-dev; bower update; bower-installer'
+            }
+        },
+        clean: {
+            post_build: [
+                'build/'
+            ],
+            pre_compress: [
+                'build/ingot/vendor/ingot/mabandit/test',
+                'build/ingot/vendor/ingot/mabandit/vendor/phpunit',
+                'build/ingot/vendor/ingot/mabandit/vendor/symfony',
+                'build/ingot/vendor/jaybizzle/crawler-detect/tests',
+                'build/ingot/vendor/blainesch/prettyarray/examples',
+                'build/ingot/vendor/bin',
+                'build/releases',
+                'build/ingot/build'
+            ]
+        },
+        run: {
+            tool: {
+                cmd: './composer'
+            }
+        },
+        copy: {
+            build: {
+                options : {
+                    mode :true
+                },
+                src: [
+                    '**',
+                    '!node_modules/**',
+                    '!releases',
+                    '!releases/**',
+                    '!.git/**',
+                    '!Gruntfile.js',
+                    '!package.json',
+                    '!.gitignore',
+                    '!.gitmodules',
+                    '!.gitattributes',
+                    '!composer.lock',
+                    '!naming-conventions.txt',
+                    '!how-to-grunt.md',
+                    '!.travis.yml',
+                    '!.scrutinizer.yml',
+                    '!phpunit.xml',
+                    '!tests/**',
+                    '!bower_components/**',
+                    '!bin/**',
+                    '!vendor/ingot/mabandit/vendor/bin/**'
+
+                ],
+                dest: 'build/<%= pkg.name %>/'
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    mode: 'zip',
+                    archive: 'releases/<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: [
+                    '**/*',
+                    '!build/*'
+                ]
+            }
+        },
+        gitadd: {
+            add_zip: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: [ 'releases/<%= pkg.name %>-<%= pkg.version %>.zip' ]
+                }
+            }
+        },
+        gittag: {
+            addtag: {
+                options: {
+                    tag: '<%= pkg.version %>',
+                    message: 'Version <%= pkg.version %>'
+                }
+            }
+        },
+        gitcommit: {
+            commit: {
+                options: {
+                    message: 'Version <%= pkg.version %>',
+                    noVerify: true,
+                    noStatus: false,
+                    allowEmpty: true
+                },
+                files: {
+                    src: [ 'package.json', 'ingot.php', 'releases/<%= pkg.name %>-<%= pkg.version %>.zip' ]
+                }
+            }
+        },
+        gitpush: {
+            push: {
+                options: {
+                    tags: true,
+                    remote: 'origin',
+                    branch: 'master'
+                }
+            }
+        },
+        replace: {
+            core_file: {
+                src: [ 'ingot.php' ],
+                overwrite: true,
+                replacements: [{
+                    from: /Version:\s*(.*)/,
+                    to: "Version: <%= pkg.version %>"
+                }, {
+                    from: /define\(\s*'INGOT_VER',\s*'(.*)'\s*\);/,
+                    to: "define( 'INGOT_VER', '<%= pkg.version %>' );"
+                }]
+            }
+        },
+        uglify: {
+            frontend: {
+                files: {
+                    'assets/front-end/js/ingot-click-test.min.js': [ 'assets/front-end/js/ingot-click-test.js' ],
+                    'assets/admin/js/ingot-post-editor.min.js' : ['assets/admin/js/ingot-post-editor.js' ]
+                }
+            },
+            admin:{
+                files: {
+                    'assets/admin/js/admin-app.min.js': [ 'assets/admin/js/admin-app.js' ]
+                }
+            }
+        },
+        watch: {
+            files: [
+                'assets/admin/js/admin-app.js',
+                'assets/front-end/js/ingot-click-test.js'
+            ],
+            tasks: ['default']
+        },
+        addtextdomain: {
+            options: {
+                textdomain: 'ignot',
+            },
+            target: {
+                files: {
+                    src: [ '*.php', '**/*.php', '!node_modules/**', '!tests/**', '!bin/**' ]
+                }
+            }
+        },
+        makepot: {
+            target: {
+                options: {
+                    domainPath: '/languages',
+                    mainFile: 'ingot.php',
+                    potFilename: 'ingot.pot',
+                    potHeaders: {
+                        poedit: true,
+                        'x-poedit-keywordslist': true
+                    },
+                    type: 'wp-plugin',
+                    updateTimestamp: true
+                }
+            }
+        },
+
+    });
+
+    //load modules
+    grunt.loadNpmTasks( 'grunt-contrib-compress' );
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-copy' );
+    grunt.loadNpmTasks( 'grunt-git' );
+    grunt.loadNpmTasks( 'grunt-text-replace' );
+    grunt.loadNpmTasks( 'grunt-shell');
+    grunt.loadNpmTasks( 'grunt-contrib-uglify');
+    grunt.loadNpmTasks( 'grunt-contrib-watch');
+    grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
+
+
+
+    //register default task
+    grunt.registerTask( 'default', [ 'uglify' ]);
+
+    //release tasks
+    grunt.registerTask( 'version_number', [ 'replace:core_file' ] );
+    grunt.registerTask( 'pre_vcs', [ 'shell:activate', 'version_number', 'copy', 'clean:pre_compress', 'compress' ] );
+    grunt.registerTask( 'do_git', [ 'gitadd', 'gitcommit', 'gittag', 'gitpush' ] );
+    grunt.registerTask( 'just_build', [  'shell:composer', 'copy', 'clean:pre_compress', 'compress' ] );
+    grunt.registerTask( 'install', [ 'shell:activate' ] );
+
+    grunt.registerTask( 'release', [ 'pre_vcs', 'do_git', 'clean:post_build' ] );
+
+
+};
---
+++ ingot.php
@@ -0,0 +1,140 @@
+<?php
+/**
+Plugin Name: Nugget by Ingot: Easy, automated and native A/B testing for everyone
+Version: 1.0.0-b-1
+Plugin URI:  http://IngotHQ.com
+Description: A nugget of Ingot, the easy A/B testing solution for WordPress
+Author:      Ingot LLC
+Author URI:  http://IngotHQ.com
+Text Domain: ingot
+Domain Path: /languages
+ */
+/**
+ * Copyright 2015-2015 Ingot LLC
+ *
+ * Licensed under the terms of the GNU General Public License version 2 or later
+ */
+
+if( ! defined( 'INGOT_VER' ) ){
+	define( 'INGOT_VER', '1.0.0-b-1' );
+	define( 'INGOT_TYPE', 'NUGGET' );
+	define( 'INGOT_URL', plugin_dir_url( __FILE__ ) );
+	define( 'INGOT_DIR', dirname( __FILE__ ) );
+	define( 'INGOT_UI_PARTIALS_DIR', dirname( __FILE__ ) . '/classes/ui/admin/partials/' );
+	define( 'INGOT_ROOT', basename( dirname( __FILE__ ) ) );
+
+
+	/**
+	 * Actions to boot up plugin
+	 *
+	 * @since 1.0.0
+	 */
+	add_action( 'plugins_loaded', 'ingot_maybe_load', 0 );
+
+	/**
+	 * Load plugin if possible
+	 *
+	 * @since 1.0.0
+	 */
+	function ingot_maybe_load() {
+		$fail = false;
+		if ( ! version_compare( PHP_VERSION, '5.4.0', '>=' ) ) {
+			$fail = true;
+			if ( is_admin() ) {
+				include_once( dirname( __FILE__ ) . '/vendor/calderawp/dismissible-notice/src/functions.php' );
+				$message = esc_html__( sprintf( 'Nugget by Ingot requires PHP version 5.4.0 or later. Current version is %s.', PHP_VERSION ), 'ingot' );
+				echo caldera_warnings_dismissible_notice( $message, true, 'activate_plugins' );
+			}
+
+		}
+		global $wp_version;
+		if ( ! version_compare( $wp_version, '4.4', '>=' ) ) {
+			$fail = true;
+			if ( is_admin() ) {
+				include_once( dirname( __FILE__ ) . '/vendor/calderawp/dismissible-notice/src/functions.php' );
+				$message = esc_html__( sprintf( 'Ingot requires WordPress version 4.4 or later. Current version is %s.', $wp_version ), 'ingot' );
+				echo caldera_warnings_dismissible_notice( $message, true, 'activate_plugins' );
+			}
+
+		}
+		if( false == $fail ){
+
+			/**
+			 * Runs before Ingot is loaded
+			 *
+			 * NOTE: Only runs if version checks pass
+			 *
+			 * @since 1.2.0
+			 */
+			do_action( 'ingot_before' );
+
+			include_once( dirname(__FILE__ ) . '/ingot_bootstrap.php' );
+			add_action( 'plugins_loaded', array( 'ingot_bootstrap', 'maybe_load' ) );
+			add_action( 'ingot_loaded', 'ingot_fs' );
+
+		}
+
+	}
+
+	/**
+	 * Load Freemius
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return \Freemius
+	 */
+	function ingot_fs() {
+		global $ingot_fs;
+
+		if ( ! isset( $ingot_fs ) ) {
+
+			require_once dirname(__FILE__) . '/vendor/freemius/wordpress-sdk/start.php';
+
+			$ingot_fs = fs_dynamic_init( array(
+				'id'                => '210',
+				'slug'              => 'ingot',
+				'public_key'        => 'pk_e6a19a3508bdb9bdc91a7182c8e0c',
+				'is_live'           => true,
+				'is_premium'        => true,
+				'has_addons'        => true,
+				'has_paid_plans'    => true,
+				'is_org_compliant'  => true,
+				'menu'              => array(
+					'slug'       => 'ingot-admin-app',
+					'support'    => false,
+					'first-path' => 'admin.php?ingot-admin-app',
+				),
+
+			) );
+
+		}
+
+		return $ingot_fs;
+
+	}
+
+
+
+
+	/**
+	 * Load translations
+	 *
+	 * @since 1.0.0
+	 */
+	add_action( 'plugins_loaded', 'ingot_load_textdomain' );
+
+	/**
+	 * Load plugin textdomain.
+	 *
+	 * @since 1.1.0
+	 */
+	function ingot_load_textdomain() {
+		load_plugin_textdomain( 'ingot', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+	}
+
+
+}
+
+
+
+
